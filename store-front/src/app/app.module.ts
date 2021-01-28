2acaf1055ebe4abb53d90521131e66536e414a37
yassine315 <yassim.lmahi@gmail.com>
2021-01-28 23:20:41 +0100

    ajout de menu

--- store-front/src/app/app.module.ts
+++ store-front/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { HeaderComponent } from './component/header/header.component';
+import { NgxPopper } from 'angular-popper';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPopper
   ],
   providers: [],
   bootstrap: [AppComponent]
